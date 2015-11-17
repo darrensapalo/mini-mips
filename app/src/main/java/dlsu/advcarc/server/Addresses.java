034d3ee0d7f22b12caef96d80b020184cea0139c
Alron <alronjan@yahoo.com>
2015-11-17 23:18:06 +0800

    Initial implementation of HTTP server. Accessing localhost:8080/mips/ now returns the index.html file.

---
+++ app/src/main/java/dlsu/advcarc/server/Addresses.java
@@ -0,0 +1,11 @@
+package dlsu.advcarc.server;
+
+/**
+ * Created by user on 11/17/2015.
+ */
+public class Addresses {
+
+    public static final String CODE_INPUT = "code.input";
+
+
+}
---
+++ app/src/main/java/dlsu/advcarc/server/handlers/HttpRequestHandler.java
@@ -0,0 +1,29 @@
+package dlsu.advcarc.server.handlers;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.Handler;
+
+/**
+ * Created by user on 11/17/2015.
+ */
+public class HttpRequestHandler implements Handler<HttpServerRequest> {
+
+    Vertx vertx;
+
+    public HttpRequestHandler(Vertx vertx){
+        this.vertx = vertx;
+    }
+
+    @Override
+    public void handle(final HttpServerRequest request) {
+        //System.out.println("Got a request with path: "+request.path());
+        if(request.path().equals("/mips")) {
+            request.response().sendFile("web/index.html");
+        }
+        else{
+            request.response().sendFile("web"+request.path().replace("mips", ""));
+        }
+    }
+
+}
